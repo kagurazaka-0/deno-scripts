51a86e2581db420bb8fecbb6068f323c05108cf2
katsuya_U <katsuya_u@icloud.com>
2022-08-26 04:28:36 +0900

    fix init-prettier

--- init-prettier.ts
+++ init-prettier.ts
@@ -1,23 +1,67 @@
 import { createNpmManager } from "./utils/createNpmManager.ts"
 import { exec } from "./utils/exec.ts"
 
-const { installPackage } = await createNpmManager()
+const { installPackage, isInstalled } = await createNpmManager()
+
+const isInstalledTailwind = isInstalled("tailwindcss")
 
 await installPackage([
-  // "prettier-plugin-organize-imports",
   "prettier",
+  "prettier-plugin-organize-imports",
   "@trivago/prettier-plugin-sort-imports",
+  isInstalledTailwind && "prettier-plugin-tailwindcss",
 ])
 
-const prettierConfig = {
+/** boolがfalseの時は`// `が付与される */
+function maybeComment(bool: boolean, source: string) {
+  if (!bool) {
+    return `// ${source}`
+  }
+  return source
+}
+
+await Deno.writeTextFile(
+  "./prettier.config.js",
+  `
+const pluginSortImports = require("@trivago/prettier-plugin-sort-imports")
+const pluginOrganizeImports = require("prettier-plugin-organize-imports")
+${maybeComment(isInstalledTailwind, `const pluginTailwindcss = require("prettier-plugin-tailwindcss")`)}
+
+const { parsers: typescriptParsers } = require("prettier/parser-typescript")
+
+/** @type {import("prettier").Parser}  */
+const myParser = {
+  ...typescriptParsers.typescript,
+  preprocess(text, options) {
+    let it = text
+    try {
+      it = pluginOrganizeImports.parsers.typescript.preprocess(it, options)
+      it = pluginSortImports.parsers.typescript.preprocess(it, options)
+    } catch (error) {
+      console.warn(\`⚠️ plugin error\`, error)
+    }
+    return it
+  },
+  ${maybeComment(isInstalledTailwind, `parse: pluginTailwindcss.parsers.typescript.parse,`)}
+}
+
+/** @type {import("prettier").Plugin}  */
+const myPlugin = {
+  parsers: {
+    typescript: myParser,
+  },
+}
+
+module.exports = {
+  plugins: [myPlugin],
   tabWidth: 2,
   semi: false,
   printWidth: 120,
   arrowParens: "always",
-  importOrder: ["^[~/]", "^[../]", "^[./]"],
+  importOrder: ["^~icons", "^[~/]", "^[../]", "^[./]"],
   importOrderSeparation: true,
 }
-
-await Deno.writeTextFile("./.prettierrc", JSON.stringify(prettierConfig))
+`
+)
 
 await exec(`npx prettier --write .`)
