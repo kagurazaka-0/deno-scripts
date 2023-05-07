912d0e93b68c838a7ec77301af4bae0694c0cbc0
katsuya_U <katsuya_u@icloud.com>
2023-05-08 06:56:25 +0900

    20230508

--- utils/exec.ts
+++ utils/exec.ts
@@ -1,19 +1,34 @@
 const DECODER = new TextDecoder()
 
-export async function exec(command: string, option: { isReturnString: true }): Promise<string>
-export async function exec(command: string, option?: { isReturnString?: false }): Promise<void>
-export async function exec(command: string, option: { isReturnString?: boolean } = {}): Promise<unknown> {
-  const { isReturnString } = option
-  const process = Deno.run({ cmd: command.split(" "), stdout: isReturnString ? "piped" : "inherit" })
+type O = {
+  isShowOutput?: boolean
+}
+
+export async function exec(command: string, option: O & { isReturnString: true }): Promise<string>
+export async function exec(command: string, option?: O & { isReturnString?: false }): Promise<void>
+export async function exec(command: string, option: O & { isReturnString?: boolean } = {}): Promise<unknown> {
+  const { isReturnString, isShowOutput } = option
+  const cmd = command.split(" ")
+  const stdout = (isReturnString ? "piped" : isShowOutput ? "inherit" : "null") satisfies Deno.RunOptions["stdout"]
+  const stderr = (isShowOutput ? "inherit" : "null") satisfies Deno.RunOptions["stderr"]
+  const process = Deno.run({ cmd, stdout, stderr })
   const status = await process.status()
   if (!status.success) {
-    console.log(`⚠️  '${command}' is end. error code: ${status.code}`)
-    Deno.exit(status.code)
+    const errorText = `'${command}' is end. error code: ${status.code}`
+    // console.log(`⚠️  ${errorText}`)
+    throw new ExecError(errorText)
   }
 
   if (isReturnString) {
     return DECODER.decode(await process.output())
   } else {
     return
+  }
+}
+
+class ExecError extends Error {
+  constructor(...params: Parameters<ErrorConstructor>) {
+    super(...params)
+    this.name = "ExecError"
   }
 }
